b5eec57d377595d84a6803e9cda3f507a6239f77
songzhao <13725570410@163.com>
2023-06-24 22:14:45 +0800

    aaa

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    name: String,
+
+    #[arg(short, long, default_value_t = 1)]
+    count: u8
+}
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("hello, {}", args.name)
+    }
 }
