0c2c9a42eddb0f5e50dc7cec6c16e0753aaff296
songzhao <13725570410@163.com>
2023-07-03 23:03:00 +0800

    last

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -48,3 +48,40 @@ pub fn get_args() -> MyResult<Config> {
     })
 }
 
+//---------------------------------------------
+
+pub fn run(config: Config) -> MyResult<()> {
+    for filename in config.files {
+        match open(&filename) {
+            Err(e) => eprintln!("{filename}:{e}"),
+            Ok(file) => {
+                let mut last_num = 0;
+                for (line_num, line_result) in file.lines().enumerate() {
+                    let line = line_result?;
+                    if config.number_lines {
+                        println!("{:6}\t{}", line_num + 1, line);
+                    } else if config.number_nonblank_lines {
+                        if !line.is_empty() {
+                            last_num += 1;
+                            println!("{:6}\t{}", last_num, line);
+                        } else {
+                            println!();
+                        }
+                    } else {
+                        println!("{}", line);
+                    }
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+//---------------------------------------------
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _   => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
